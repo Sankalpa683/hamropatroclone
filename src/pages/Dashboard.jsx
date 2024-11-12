12ca815f4722edb20c4c0c53cd763c97ebdec355
Sankalpa683 <sank.dahal@gmail.com>
2024-11-12 21:05:10 +0545

    Adding the date, day, month & year value dyanamically

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,35 @@
+import NepaliDate from 'nepali-date-converter'
+
+//  Dyanamic value of current day, month & year 
+// source: https://www.w3schools.com/js/js_dates.asp
+
+let date = new Date();
+let year = date.getFullYear();
+let day = date.getDate();
+
+// to get day name based on day value (eg: 3 = tuesday) => chatgpt response
+
+const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// currentDayName is being assigned based on where the dayNames month name array value is positioned
+
+const currentDayName = dayNames[day];
+
+let months = date.getMonth();
+// to get month name based on month value (eg: 12 = november) => chatgpt response
+
+const monthNames = [
+  "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+// currentMonthName is being assigned based on where the monthNames month name array value is positioned
+
+const currentMonthName = monthNames[months];
+
+
+
+
+
 export default function Dashboard() {
   return (
     <>
@@ -9,7 +41,7 @@ export default function Dashboard() {
                 <div className="logo">
                   <div className="date">
                     <span className="nep text-3xl">
-                      27 Kartik 2081,<br /> Tuesday
+                      {day} {currentMonthName} {year},<br /> {currentDayName}
                     </span>
                   </div>
                   <div style={{ margin: '10px 0', color: 'white', fontSize: '1.3rem' }}>
@@ -20,12 +52,12 @@ export default function Dashboard() {
                   </div>
                   <div className="time">
                     <span> दिउँसोको ०२ : १७</span><br />
-                    <span>Nov 12, 2024</span>
+                    <span>Nov {day}, {year}</span>
                     <br />
                   </div>
                 </div>
               </div>
-              <div className="">
+              <div className=" mx-auto">
                 <img src="https://www.hamropatro.com/images/rotating-ads//bank1.gif" className="w-[800px]" />
               </div>
             </div>
